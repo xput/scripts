3d757c54a8ba67ddd98d4b5de4d56a09951a13e0
André Dieckmann <ad@xput.net>
2025-05-23 21:06:29 +0200

    change hetzner failover

--- hetzner_switch_virtualip/failover.pl
+++ hetzner_switch_virtualip/failover.pl
@@ -58,7 +58,7 @@ my $ip_server_a = $ENV{IP_SERVER_A};
 my $ip_server_b = $ENV{IP_SERVER_B};
 my $name_server_a = $ENV{NAME_SERVER_A};
 my $name_server_b = $ENV{NAME_SERVER_B};
-my ($master, $master_name, $slave, $slave_name);
+my ($primary, $primary_name, $secondary, $secondary_name);
 
 #---------------------------#
 #   Main logic
@@ -69,17 +69,17 @@ if ($help) {
 elsif ($info) {
     get_info();
     log_msg("virtual: $ip_virtual");
-    log_msg("master : $master ($master_name)");
-    log_msg("slave  : $slave ($slave_name)");
+    log_msg("primary : $primary ($primary_name)");
+    log_msg("secondary  : $secondary ($secondary_name)");
 }
 elsif ($force) {
     get_info();
-    if (system("ssh -p22022 root\@$slave service openvpn start") != 0) {
-        log_msg("Error: Failed to start OpenVPN on $slave");
+    if (system("ssh -p22022 root\@$secondary service openvpn start") != 0) {
+        log_msg("Error: Failed to start OpenVPN on $secondary");
         exit 5;
     }
-    switch_to($slave);
-    log_msg("Switched to slave: $slave ($slave_name)");
+    switch_to($secondary);
+    log_msg("Switched to secondary: $secondary ($secondary_name)");
 } 
 else {
     if (ping_check($ip_internal)) {
@@ -89,16 +89,16 @@ else {
         exit 2;
     } else {
         get_info();
-        unless (ping_check($slave)) {
-            log_msg("Neither master nor slave is reachable!");
+        unless (ping_check($secondary)) {
+            log_msg("Neither primary nor secondary is reachable!");
             exit 4;
         }
-        if (system("ssh -p22022 root\@$slave service openvpn start") != 0) {
-            log_msg("Error: Failed to start OpenVPN on $slave");
+        if (system("ssh -p22022 root\@$secondary service openvpn start") != 0) {
+            log_msg("Error: Failed to start OpenVPN on $secondary");
             exit 5;
         }
-        switch_to($slave);
-        log_msg("Switched from $master ($master_name) to $slave ($slave_name)");
+        switch_to($secondary);
+        log_msg("Switched from $primary ($primary_name) to $secondary ($secondary_name)");
     }
 }
 
@@ -119,17 +119,17 @@ sub get_info {
         1;
     } or die "Failed to parse JSON response";
 
-    $master = $data->{failover}{active_server_ip};
-    if ($ip_server_a ne $master) {
-        $slave = $ip_server_a;
-        $slave_name = $name_server_a;
-        $master_name = $name_server_b;
-    } elsif ($ip_server_b ne $master) {
-        $slave = $ip_server_b;
-        $slave_name = $name_server_b;
-        $master_name = $name_server_a;
+    $primary = $data->{failover}{active_server_ip};
+    if ($ip_server_a ne $primary) {
+        $secondary = $ip_server_a;
+        $secondary_name = $name_server_a;
+        $primary_name = $name_server_b;
+    } elsif ($ip_server_b ne $primary) {
+        $secondary = $ip_server_b;
+        $secondary_name = $name_server_b;
+        $primary_name = $name_server_a;
     } else {
-        die "Unable to determine master/slave status";
+        die "Unable to determine primary/secondary status";
     }
 }
 
@@ -145,17 +145,17 @@ sub switch_to {
     my $data = eval { decode_json($res->content) } 
         or die "Failed to parse JSON response";
 
-    $master = $data->{failover}{active_server_ip};
-    if ($ip_server_a ne $master) {
-        $slave = $ip_server_a;
-        $slave_name = $name_server_a;
-        $master_name = $name_server_b;
-    } elsif ($ip_server_b ne $master) {
-        $slave = $ip_server_b;
-        $slave_name = $name_server_b;
-        $master_name = $name_server_a;
+    $primary = $data->{failover}{active_server_ip};
+    if ($ip_server_a ne $primary) {
+        $secondary = $ip_server_a;
+        $secondary_name = $name_server_a;
+        $primary_name = $name_server_b;
+    } elsif ($ip_server_b ne $primary) {
+        $secondary = $ip_server_b;
+        $secondary_name = $name_server_b;
+        $primary_name = $name_server_a;
     } else {
-        die "Unable to determine new master/slave status";
+        die "Unable to determine new primary/secondary status";
     }
 }
 
@@ -172,12 +172,12 @@ sub ping_check {
 sub help {
     print STDERR <<'USAGE';
 
-Switch master server on Hetzner failover IP.
+Switch primary server on Hetzner failover IP.
 
-Usage: script.pl [options]
+Usage: failover.pl [options]
 
     -i --info     Show current status
-    -f --force    Force switch to slave
+    -f --force    Force switch to secondary
     -h --help     Display this help
 
 USAGE
